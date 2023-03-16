ceac076db9ba848d80a9439d642e1e27da65e474
bye0607 <bye0607@gmail.com>
2023-03-16 17:13:44 +0900

    kej, 홍보사이트 backend commit

---
+++ GlowUp_Brand/gu_back/src/main/java/com/glowup/brand/GuBackApplication.java
@@ -0,0 +1,13 @@
+package com.glowup.brand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuBackApplication.class, args);
+	}
+
+}
